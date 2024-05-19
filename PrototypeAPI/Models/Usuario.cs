27d6448adf0f7b062dec62512a1e5f7ed0f137e5
cicerocmss <ssfs.cicero@gmail.com>
2024-05-19 15:18:24 -0300

    Update Usuario.cs
    ADICIONANDO COMENTARIO PARA TESTAR COMPORTAMENTO DO VISUAL STUDIO NA MAQUINA LOCAL.

--- PrototypeAPI/Models/Usuario.cs
+++ PrototypeAPI/Models/Usuario.cs
@@ -5,6 +5,6 @@
         public int Id { get; set; }
         public string? Nome { get; set; }  // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
         public string? Senha { get; set; } // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
-        public int? Tipo { get; set; }
+        public int? Tipo { get; set; } // TESTANDO ALTERAÇÃO NA PÁGINA DO GITHUB PARA VERIFICAR COMO O VS DETECTA NA BRANCH LOCAL 
     }
 }
