e7058c6dd3416c1807645af76225ceec0278c6cd
CMSS <ssfs.cicero@gmail.com>
2024-05-19 13:54:05 -0300

    Criada a branch feature/implementacao-classe-reserva

--- PrototypeAPI/Models/Usuario.cs
+++ PrototypeAPI/Models/Usuario.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public string? Nome { get; set; }  // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
         public string? Senha { get; set; } // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
+        public int? Tipo { get; set; }
     }
 }
