93aeaf65b905c02b85280051bcdd941c9fbd9de3
CMSS <ssfs.cicero@gmail.com>
2024-05-19 15:32:15 -0300

    Adicionada a classe Transacao em Controllers.

---
+++ PrototypeAPI/Controllers/Transacao.cs
@@ -0,0 +1,10 @@
+﻿using PrototypeAPI.Models;
+
+namespace PrototypeAPI.Controllers
+{
+    public class Transacao
+    {
+        //AQUI SERÃO ADICIONADAS TODAS AS FUNÇÕES DE TRANSACAO E DETALHES DESSAS TRANSACOES
+        Usuario? usuario;
+    }
+}
