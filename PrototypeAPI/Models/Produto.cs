e0fc6cf1a988ba7477ab8171a9c9ce2b379fe2d7
NBCMSS <NBCMSS@DESKTOP-TEI6A0V>
2024-05-19 11:24:25 -0300

    Adicionar arquivos de projeto.

---
+++ PrototypeAPI/Models/Produto.cs
@@ -0,0 +1,10 @@
+﻿namespace PrototypeAPI.Models
+{
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; } // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
+        public string? Descricao { get; set; } // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
+        public string? Preco { get; set; } // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
+    }
+}
---
+++ PrototypeAPI/Models/Usuario.cs
@@ -0,0 +1,9 @@
+﻿namespace PrototypeAPI.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string? Nome { get; set; }  // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
+        public string? Senha { get; set; } // O SIMBOLO '?' INDICA QUE A STRING PODE RECEBER 'NULL' COMO VALOR
+    }
+}
